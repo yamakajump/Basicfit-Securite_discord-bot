f874836d8c7872a9236f98541f842c259627cf07
CyberTrist <114986392+CyberSpaceRS@users.noreply.github.com>
2025-01-09 14:25:19 +0100

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -4,6 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('unban')
         .setDescription("Débannir un utilisateur du serveur.")
+        .setDefaultMemberPermissions('32'), // = Gérer le serveur
         .addStringOption(option =>
             option.setName('user_id')
                 .setDescription("L'ID de l'utilisateur à débannir.")
