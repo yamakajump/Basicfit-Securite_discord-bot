e9822ed2ae2c77526b6fa8e65635997a1dbd77d4
CyberTrist <114986392+CyberSpaceRS@users.noreply.github.com>
2025-01-09 14:25:01 +0100

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -4,6 +4,7 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('slowmode')
         .setDescription("Activer ou modifier le mode lent d'un salon.")
+        .setDefaultMemberPermissions('32'), // = Gérer le serveur
         .addIntegerOption(option =>
             option.setName('temps')
                 .setDescription("Durée du mode lent en secondes (0 pour désactiver).")
